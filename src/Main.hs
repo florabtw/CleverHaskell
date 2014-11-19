14a893ec4929c66728712d0f87ae3f327a5d2bca
Nick Pierson <nkcrpn@gmail.com>
2014-11-18 19:35:25 -0600

    Adds JSON parsing of response

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Network.HTTP.Conduit
 import Data.String (fromString)
+import Data.Aeson
+import Control.Applicative
+import Control.Monad
+import qualified Data.ByteString.Lazy.Internal as B
 
-main = getSections
+data AllData = AllData { getData :: [SectionData] }
+                       deriving (Show)
+
+data SectionData = SectionData { getSection :: Section }
+                               deriving (Show)
+
+data Section = Section { getId :: String }
+                       deriving (Show)
+
+-- List of things that are dumb:
+--              1. This
+instance FromJSON Section where
+    parseJSON (Object v) =
+        Section <$> v .: "id"
+    parseJSON _ = mzero
+
+instance FromJSON AllData where
+    parseJSON (Object v) =
+        AllData <$> v .: "data"
+    parseJSON _ = mzero
+
+instance FromJSON SectionData where
+    parseJSON (Object v) =
+        SectionData <$> v .: "data"
+    parseJSON _ = mzero
+-- /dumb list
+
+main = undefined
 
 server :: String
 server = "https://api.clever.com/v1.1"
@@ -14,9 +47,18 @@ sections = server ++ "/sections"
 studentsInSection :: String -> String
 studentsInSection x = sections ++ "/" ++ x ++ "/students"
 
-getSections = do
+requestSections :: IO B.ByteString
+requestSections = do
     request <- parseUrl sections
     let headers = [(fromString "Authorization", fromString "Bearer DEMO_TOKEN")]
         request' = request { requestHeaders = headers }
     res <- withManager $ httpLbs request'
-    print res
+    return $ responseBody res
+
+parseSections :: B.ByteString -> Maybe [SectionData]
+parseSections sectionJson = do
+    decoded <- decode sectionJson
+    return $ getData decoded
+
+getSectionIds :: [SectionData] -> [String]
+getSectionIds sectionData = map (getId . getSection) sectionData
